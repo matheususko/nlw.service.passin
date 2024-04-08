7488875b6310cd3762786b6f4eca70a74a3fa498
Matheus Susko <matheususko@gmail.com>
2024-04-08 17:48:15 -0300

    new: change health route

---
+++ src/routes/health.ts
@@ -0,0 +1,8 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+
+export const health = async (app: FastifyInstance) => {
+  app.withTypeProvider<ZodTypeProvider>().get('/healthz', async (request, reply) => {
+    return reply.status(200).send('OK!')
+  })
+}
--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,28 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
-import fastifySwagger from "@fastify/swagger";
-import fastifySwaggerUI from "@fastify/swagger-ui";
-import fastifyCors from "@fastify/cors";
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUI from '@fastify/swagger-ui'
+import fastifyCors from '@fastify/cors'
 
-import { serializerCompiler, validatorCompiler, jsonSchemaTransform, ZodTypeProvider } from 'fastify-type-provider-zod'
-import { createEvent } from "./routes/create-event";
-import { registerForEvent } from "./routes/register-for-event";
-import { getEvent } from "./routes/get-event";
-import { getAttendeeBadge } from "./routes/get-attendee-badge";
-import { checkIn } from "./routes/check-in";
-import { getEventAttendees } from "./routes/get-event-attendees";
-import { errorHandler } from "./error-handler";
+import {
+  serializerCompiler,
+  validatorCompiler,
+  jsonSchemaTransform,
+  ZodTypeProvider
+} from 'fastify-type-provider-zod'
+import { createEvent } from './routes/create-event'
+import { registerForEvent } from './routes/register-for-event'
+import { getEvent } from './routes/get-event'
+import { getAttendeeBadge } from './routes/get-attendee-badge'
+import { checkIn } from './routes/check-in'
+import { getEventAttendees } from './routes/get-event-attendees'
+import { errorHandler } from './error-handler'
+import { health } from './routes/health'
 
 export const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
-  origin: '*',
+  origin: '*'
 })
 
 app.register(fastifySwagger, {
@@ -25,19 +31,20 @@ app.register(fastifySwagger, {
     produces: ['application/json'],
     info: {
       title: 'pass.in',
-      description: 'Especificações da API para o back-end da aplicação pass.in construída durante o NLW Unite da Rocketseat.',
+      description:
+        'Especificações da API para o back-end da aplicação pass.in construída durante o NLW Unite da Rocketseat.',
       version: '1.0.0'
-    },
+    }
   },
-  transform: jsonSchemaTransform,
+  transform: jsonSchemaTransform
 })
 
 app.register(fastifySwaggerUI, {
-  routePrefix: '/docs',
+  routePrefix: '/docs'
 })
 
-app.setValidatorCompiler(validatorCompiler);
-app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
 
 app.register(createEvent)
 app.register(registerForEvent)
@@ -45,6 +52,7 @@ app.register(getEvent)
 app.register(getAttendeeBadge)
 app.register(checkIn)
 app.register(getEventAttendees)
+app.register(health)
 
 app.setErrorHandler(errorHandler)
 
