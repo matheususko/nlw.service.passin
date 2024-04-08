1036553724dc10e2757f141418384ba137c991d3
Matheus Susko <matheususko@gmail.com>
2024-04-08 18:19:19 -0300

    new: change health route

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -3,6 +3,6 @@ import { ZodTypeProvider } from 'fastify-type-provider-zod'
 
 export const health = async (app: FastifyInstance) => {
   app.withTypeProvider<ZodTypeProvider>().get('/healthz', async (request, reply) => {
-    return reply.status(200).send('OK!')
+    return reply.status(200).send('OKK!')
   })
 }
